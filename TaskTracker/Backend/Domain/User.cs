229952273f3a68191c6059fe75fb786aed0fc50d
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-04-21 14:28:30 -0300

    GREEN: Dont put an arroba in Email, return an exception

--- TaskTracker/Backend/Domain/User.cs
+++ TaskTracker/Backend/Domain/User.cs
@@ -34,6 +34,7 @@ public class User
         set
         {
             if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("The email cannot be empty");
+            if (!value.Contains("@")) throw new ArgumentException("Error in the email format");
             _email = value;
         }
         
