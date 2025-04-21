de5f759c0e2fb20d44324598e969824828b4c16b
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-04-21 20:07:09 -0300

    RED: adding a title null to a project returns an exception

--- TaskTracker/BackendTest/DomainTest/TaskTest.cs
+++ TaskTracker/BackendTest/DomainTest/TaskTest.cs
@@ -21,8 +21,14 @@ public class TaskTest
     [TestMethod]
     public void CreateTitleForTask()
     {
-
         _task.ValidateTitle = "Tittle2";
         Assert.AreEqual("Tittle2", _task.ValidateTitle);
+    }
+
+    [TestMethod]
+    public void IPutTitleNullReturnsAnException()
+    {
+        ArgumentException ex = Assert.ThrowsException<ArgumentException>(() => _task.ValidateTitle = null);
+        Assert.AreEqual("The title name cannot be empty", _task.ValidateTitle);
     }
 }
