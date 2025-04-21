31fcbef16d00c4a06c5e52dbfafc21174fd7ae11
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-04-21 18:53:46 -0300

    REFACTOR: add password without special character to user, return an exception

--- TaskTracker/Backend/Domain/User.cs
+++ TaskTracker/Backend/Domain/User.cs
@@ -61,8 +61,12 @@ public class User
         get => _password;
         set
         {
-            if (string.IsNullOrWhiteSpace(value) || !value.Any(char.IsUpper) || !value.Any(char.IsLower)) throw new ArgumentException("The password format is not correct");
-            if(value.Equals("Pedro1234")) throw new ArgumentException("The password format is not correct");
+            if (string.IsNullOrWhiteSpace(value) || !value.Any(char.IsUpper) || !value.Any(char.IsLower) ||
+                !value.Any(c => char.IsSymbol(c) || char.IsPunctuation(c)))
+            {
+                throw new ArgumentException("The password format is not correct");
+            }
+
             _password = value;
         }
     }
