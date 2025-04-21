a0252d634726cb49c733e34cb3452489a6a2e42f
Felipe <felipemuzio02@gmail.com>
2025-04-21 17:35:53 -0300

    RED: create proyect

---
+++ TaskTracker/BackendTest/DomainTest/ProyectTest.cs
@@ -0,0 +1,12 @@
+namespace BackendTest.DomainTest;
+
+[TestClass]
+public class ProyectTest
+{
+    
+    [TestMethod]
+    public void OnInitialize()
+    {
+        Proyect proyect = new Proyect();
+    }
+}
