3a649fa8ee07283bc94600ef658b346313781093
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-04-21 14:35:38 -0300

    REFACTOR: Dont put an arroba in Email, return an exception

--- TaskTracker/Backend/Domain/User.cs
+++ TaskTracker/Backend/Domain/User.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Backend.Domain;
 
 public class User
@@ -34,7 +36,7 @@ public class User
         set
         {
             if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("The email cannot be empty");
-            if (!value.Contains("@")) throw new ArgumentException("Error in the email format");
+            if (!Regex.IsMatch(value, "@")) throw new ArgumentException("Error in the email format");
             _email = value;
         }
         
