4e1953e2fea8488994a00b4cf4fe10927228a899
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-04-24 18:56:02 -0300

    GREEN: adding a Project to Critical Path

--- TaskTracker/Backend/Domain/CriticalPath.cs
+++ TaskTracker/Backend/Domain/CriticalPath.cs
@@ -3,5 +3,14 @@
 public class CriticalPath
 {
     private Project _project;
+    
+    public Project Project
+    {
+        get => _project;
+        set
+        {
+            _project = value;
+        }
+    }
 
 }
