74983af46e4bbd525207a2787961a289e4bd666e
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-04-25 15:00:28 -0300

    RED: create a project role

---
+++ TaskTracker/BackendTest/DomainTest/ProjectRoleTest.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendTest.DomainTest
+{
+    [TestClass]
+    public class ProjectRoleTest
+    {
+        [TestMethod]
+        public void CreateProjectRoleTest()
+        {
+            ProjectRole projectRole = new ProjectRole();
+        }
+    }
+}
