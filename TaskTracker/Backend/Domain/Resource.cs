8d3c3c6a51dab2c906ab0babfe824e8441b8197c
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-04-25 12:48:06 -0300

    GREEN: Put to a resource a description

--- TaskTracker/Backend/Domain/Resource.cs
+++ TaskTracker/Backend/Domain/Resource.cs
@@ -3,6 +3,7 @@ namespace Backend.Domain;
 public class Resource
 {
     private string _name;
+    private string _description;
     
     public string Name
     {
@@ -12,5 +13,11 @@ public class Resource
             if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("The resource name cannot be empty");
             _name = value;
         }
+    }
+    
+    public string Description
+    {
+        get => _description;
+        set => _description = value;
     }
 }
