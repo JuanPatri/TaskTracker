4a070c89a81f0aebfcc4d75a81175cae218bd479
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-04-28 08:31:03 -0300

    GREEN: adding  Project to Project Role

--- TaskTracker/Backend/Domain/ProjectRole.cs
+++ TaskTracker/Backend/Domain/ProjectRole.cs
@@ -4,11 +4,20 @@ using Enums;
 public class ProjectRole
 {
     private RoleType _roleType;
-
+    private List<Project> _project;
     public RoleType RoleType
     {
         get => _roleType;
         set =>  _roleType = value;
+    }
+
+    public List<Project> Project
+    {
+        get => _project;
+        set
+        {
+            _project = value;
+        }
     }
     
 }
