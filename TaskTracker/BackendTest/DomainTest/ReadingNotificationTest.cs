51d3e887a9c8d267561d882a7a738cf6f075c875
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-04-30 18:46:37 -0300

    RED: create reading notification

---
+++ TaskTracker/BackendTest/DomainTest/ReadingNotificationTest.cs
@@ -0,0 +1,10 @@
+namespace BackendTest.DomainTest;
+
+public class ReadingNotificationTest
+{
+    [TestMethod]
+    public void CreateNotificationTest()
+    {
+        ReadingNotification _readingNotification = new ReadingNotification();
+    }
+}
