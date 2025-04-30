072be094dbafa55c0126ae7cb6209b483b19da16
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-04-30 14:23:35 -0300

    GREEN: add date to notification

--- TaskTracker/Backend/Domain/Notification.cs
+++ TaskTracker/Backend/Domain/Notification.cs
@@ -3,6 +3,7 @@ namespace Backend.Domain;
 public class Notification
 {
     private string _message;
+    private DateTime _date;
     
     public string Message
     {
@@ -12,5 +13,11 @@ public class Notification
             if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException("The notification message cannot be empty");
             _message = value;
         } 
+    }
+    
+    public DateTime Date
+    {
+        get => _date;
+        set => _date = value;
     }
 }
