f4445b5c39920a0efcae25e6a600c14ee48cd5eb
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-04-30 11:20:30 -0300

    GREEN: add message to notification

--- TaskTracker/Backend/Domain/Notification.cs
+++ TaskTracker/Backend/Domain/Notification.cs
@@ -2,5 +2,11 @@ namespace Backend.Domain;
 
 public class Notification
 {
+    private string _message;
     
+    public string Message
+    {
+        get => _message;
+        set => _message = value;
+    }
 }
