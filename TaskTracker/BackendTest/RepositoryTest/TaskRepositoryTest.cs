866b75940385ef849f12cb2270e3e7e3c7eca66e
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-05-01 20:17:16 -0300

    RED: create TaskRepository

---
+++ TaskTracker/BackendTest/RepositoryTest/TaskRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿namespace BackendTest.RepositoryTest;
+using Backend.Domain;
+
+
+[TestClass]
+public class TaskRepositoryTest
+{
+    
+    private TaskRepository _taskRepository;
+    private Task _task;
+    
+    [TestInitialize]
+    public void OnInitialize()
+    {
+        _taskRepository = new TaskRepository();
+        _task = new Task();
+        _task.Title = "Task 1";
+    }
+    
+    [TestMethod]
+    public void CreateTaskRepository()
+    { 
+        Assert.IsNotNull(_taskRepository);
+    }
+
+}
