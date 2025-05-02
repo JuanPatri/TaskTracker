7bced3c7af491a3747e8a19a37e671bc1efb9444
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-05-02 18:20:09 -0300

    edit registerUser to add de user to de repository and edit program.cs

--- TaskTracker/Frontend/Program.cs
+++ TaskTracker/Frontend/Program.cs
@@ -1,27 +1,28 @@
+using Backend.Service;
+using Backend.Repository;
+using Backend.Domain;
 using Frontend.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddScoped<IRepository<User>, UserRepository>(); 
+builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
