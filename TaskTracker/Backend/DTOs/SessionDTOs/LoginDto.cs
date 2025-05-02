fb789226e934f6eeae366aa5b41451e3bd9f1851
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-05-02 19:49:29 -0300

    GREEN: create LoginDto

---
+++ TaskTracker/Backend/DTOs/SessionDTOs/LoginDto.cs
@@ -0,0 +1,17 @@
+using Backend.Domain;
+
+namespace Backend.DTOs.SessionDTOs;
+public class LoginDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public User ToEntity()
+    {
+        return new User
+        {
+            Email = Email,
+            Password = Password
+        };
+    }
+}
