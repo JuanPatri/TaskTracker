34c580dc1b83ba4839fc4e94bd3d9ea60743a924
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-05-03 19:27:05 -0300

    GREEN: create to entity

--- TaskTracker/Backend/DTOs/TaskDTOs/GetTaskDTO.cs
+++ TaskTracker/Backend/DTOs/TaskDTOs/GetTaskDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace Backend.DTOs.TaskDTOs;
+﻿using Task = Backend.Domain.Task;
+
+namespace Backend.DTOs.TaskDTOs;
 
 public class GetTaskDTO
 {
     public string Title { get; set; } = string.Empty;
+    
+    public Task ToEntity()
+    {
+        return new Task()
+        {
+            Title = Title
+        };
+    }
 }
