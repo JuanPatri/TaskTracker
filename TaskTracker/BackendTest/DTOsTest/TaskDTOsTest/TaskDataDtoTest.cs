2fce8520f1d1e706e20c5b348b37ae0ca28aaa50
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-05-03 15:40:54 -0300

    RED: set description  for task in TaskDataDTO

--- TaskTracker/BackendTest/DTOsTest/TaskDTOsTest/TaskDataDtoTest.cs
+++ TaskTracker/BackendTest/DTOsTest/TaskDTOsTest/TaskDataDtoTest.cs
@@ -19,4 +19,11 @@ public class TaskDataDtoTest
         _taskDataDto.Title = "Task 1";
         Assert.AreEqual("Task 1", _taskDataDto.Title);
     }
+
+    [TestMethod]
+    public void SetDescriptionForTask()
+    {
+        _taskDataDto.Description = "Description of Task 1";
+        Assert.AreEqual("Description of Task 1", _taskDataDto.Description);
+    }
 }
