1959ecaa30a0378a0855ee23b2993825a5216f49
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-05-03 17:01:37 -0300

    RED: create Task whith TaskDataDTO

--- TaskTracker/Backend/DTOs/TaskDTOs/TaskDataDTO.cs
+++ TaskTracker/Backend/DTOs/TaskDTOs/TaskDataDTO.cs
@@ -1,5 +1,6 @@
 ﻿using Backend.Domain.Enums;
-using Backend.Domain;
+using Task = Backend.Domain.Task;
+using Backend.Domain.Repository;
 
 namespace Backend.DTOs.TaskDTOs;
 
@@ -11,4 +12,22 @@ public class TaskDataDTO
     public Status Status { get; set; } = Status.Pending;
     public int Project { get; set; } = 0;
     public List<string> Dependencies { get; set; } = new List<string>();
+    
+    public Task ToEntity()
+    {
+        List<Task> taskDependencies = new List<Task>();
+        for(int i = 0; i < Dependencies.Count; i++)
+        {
+            taskDependencies.Add(_repositoryProject.Find(t => t.title == Dependencies[i])); 
+        }
+        return new Task()
+        {
+            Title = Title,
+            Description = Description,
+            Duration = Duration,
+            Status = Status,
+            Project = _repositoryProject.Find(p => p.id  == Project),
+            Dependencies = taskDependencies
+        };
+    }
 }
