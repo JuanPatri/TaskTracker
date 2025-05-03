49de167ed351ef1a89adedc87cd2d7d3686cce73
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-05-03 15:48:08 -0300

    GREEN: set duration for task in TaskDataDTO

--- TaskTracker/Backend/DTOs/TaskDTOs/TaskDataDTO.cs
+++ TaskTracker/Backend/DTOs/TaskDTOs/TaskDataDTO.cs
@@ -6,4 +6,5 @@ public class TaskDataDTO
 { 
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public TimeSpan Duration { get; set; } = TimeSpan.Zero;
 }
