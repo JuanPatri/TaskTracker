541e5f5e2912c4e27abcbc623cf12456ad1098d7
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 12:02:36 -0300

    GREEN: Create description for project dto

--- TaskTracker/Backend/DTOs/ProjectDTOs/ProjectDataDTO.cs
+++ TaskTracker/Backend/DTOs/ProjectDTOs/ProjectDataDTO.cs
@@ -3,4 +3,5 @@ namespace Backend.DTOs.ProjectDTOs;
 public class ProjectDataDTO
 {
     public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 }
