4c0828b899a19460a6f18f6ba0eebbd2729ec1fb
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 12:10:19 -0300

    GREEN: Add finish date for project dto

--- TaskTracker/Backend/DTOs/ProjectDTOs/ProjectDataDTO.cs
+++ TaskTracker/Backend/DTOs/ProjectDTOs/ProjectDataDTO.cs
@@ -5,4 +5,5 @@ public class ProjectDataDTO
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public DateTime StartDate { get; set; } = DateTime.Today;
+    public DateTime FinishDate { get; set; } = DateTime.Today;
 }
