ab6e266cf69db31ef288f4a414addbe28912fefb
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-05-04 01:46:58 -0300

    RED: update resource

--- TaskTracker/BackendTest/RepositoryTest/ResourceRepositoryTest.cs
+++ TaskTracker/BackendTest/RepositoryTest/ResourceRepositoryTest.cs
@@ -35,4 +35,20 @@ public class ResourceRepositoryTest
         _resourceRepository.Add(_resource);
         Assert.AreEqual(_resourceRepository.FindAll().Count, 1);
     }
+    
+    [TestMethod]
+    public void UpdateExistingResourceUpdatesFieldsCorrectly()
+    {
+        _resourceRepository.Add(_resource);
+        Assert.AreEqual(_resource.Name, "new resource");
+        
+        Resource updateResource = new Resource()
+        {
+            Name = "UpdatedName",
+            Description = "UpdatedDescription",
+        };
+        
+        _resourceRepository.Update(updateResource);
+        Assert.AreEqual(_resource.Name, "UpdatedName");
+    }
 }
