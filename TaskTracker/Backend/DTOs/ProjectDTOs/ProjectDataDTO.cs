2a36c7f87a349f04be9910effaee4f8dacc738fe
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 12:08:20 -0300

    GREEN: Add start date for project dto

--- TaskTracker/Backend/DTOs/ProjectDTOs/ProjectDataDTO.cs
+++ TaskTracker/Backend/DTOs/ProjectDTOs/ProjectDataDTO.cs
@@ -4,4 +4,5 @@ public class ProjectDataDTO
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; } = DateTime.Today;
 }
