068169e839977263a19dd4703c13ed9a1e2a7f2c
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 11:57:25 -0300

    RED: Create description for project dto

--- TaskTracker/BackendTest/DTOsTest/ProjectDTOsTest/ProjectDataDtoTest.cs
+++ TaskTracker/BackendTest/DTOsTest/ProjectDTOsTest/ProjectDataDtoTest.cs
@@ -13,4 +13,12 @@ public class ProjectDataDtoTest
         Assert.AreEqual("Project A", projectDto.Name);
     }
     
+    [TestMethod]
+    public void CreateDescriptionForProjectTest()
+    {
+        ProjectDataDTO projectDto = new ProjectDataDTO();
+        projectDto.Description = "This is a test project.";
+        Assert.AreEqual("This is a test project.", projectDto.Description);
+    }
+    
 }
