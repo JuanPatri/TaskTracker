be398a4aa9cf09ad2438bbf065b778d426f0682f
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 11:37:04 -0300

    RED: Create name for project dto

---
+++ TaskTracker/BackendTest/DTOsTest/ProjectDTOsTest/ProjectDataDtoTest.cs
@@ -0,0 +1,14 @@
+namespace BackendTest.DTOsTest.ProjectDTOsTest;
+
+[TestClass]
+public class ProjectDataDtoTest
+{
+    [TestMethod]
+    public void CreateNameForProject()
+    {
+        ProjectDataDTO projectDto = new ProjectDataDTO();
+        projectDto.Name = "Project A";
+        Assert.AreEqual("Project A", projectDto.Name);
+    }
+    
+}
