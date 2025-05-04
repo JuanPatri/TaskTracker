9a59fb801779c52ac245d2723c973ca04a931d31
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 13:12:42 -0300

    GREEN: Add toEntity to project getDto

--- TaskTracker/Backend/DTOs/ProjectDTOs/GetProjectDTO.cs
+++ TaskTracker/Backend/DTOs/ProjectDTOs/GetProjectDTO.cs
@@ -1,6 +1,16 @@
+using Backend.Domain;
+
 namespace Backend.DTOs.ProjectDTOs;
 
 public class GetProjectDTO
 {
     public string Name { get; set; } = string.Empty;
+    
+    public Project ToEntity()
+    {
+        return new Project() 
+        {
+            Name = Name
+        };
+    }
 }
