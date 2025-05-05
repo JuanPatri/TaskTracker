df2b9991426c42df329896a20957eb3fa4868690
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 23:13:46 -0300

    Add id in project service

--- TaskTracker/Backend/Service/ProjectService.cs
+++ TaskTracker/Backend/Service/ProjectService.cs
@@ -7,26 +7,33 @@ namespace Backend.Service;
 public class ProjectService
 {
     private readonly IRepository<Project> _projectRepository;
+    private int _id;
     
     public ProjectService(IRepository<Project> projectRepository)
     {
         _projectRepository = projectRepository;
+        _id = 2;
     }
     
     public Project AddProject(ProjectDataDTO project)
     {
-        Project createdProject = project.ToEntity();
-        return _projectRepository.Add(createdProject);
+        if (_projectRepository.Find(p => p.Name == project.Name) != null)
+        {
+            throw new ArgumentException("Project with the same name already exists");
+        }
+        project.Id = _id++;
+        Project? createdProject = _projectRepository.Add(project.ToEntity());
+        return createdProject;
     }
     
     public void RemoveProject(GetProjectDTO project)
     {
-        _projectRepository.Delete(project.Name);
+        _projectRepository.Delete(project.Id.ToString());
     }
     
     public Project? GetProject(GetProjectDTO project)
     {
-        return _projectRepository.Find(p => p.Name == project.Name);
+        return _projectRepository.Find(p => p.Id == project.Id);
     }
     
     public List<Project> GetAllProjects()
@@ -36,7 +43,7 @@ public class ProjectService
     
     public Project? UpdateProject(ProjectDataDTO projectDto)
     {
-        Project updatedProject = projectDto.ToEntity();
-        return _projectRepository.Update(updatedProject);
+        Project? updatedProject = _projectRepository.Update(projectDto.ToEntity());
+        return updatedProject;
     }
 }
