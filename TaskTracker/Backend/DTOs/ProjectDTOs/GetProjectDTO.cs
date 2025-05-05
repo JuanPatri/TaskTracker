39929bdadb9f40722d045c8b52b0b9610dc540d4
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-05-04 22:29:12 -0300

    Add id to GetProjectDto

--- TaskTracker/Backend/DTOs/ProjectDTOs/GetProjectDTO.cs
+++ TaskTracker/Backend/DTOs/ProjectDTOs/GetProjectDTO.cs
@@ -4,12 +4,14 @@ namespace Backend.DTOs.ProjectDTOs;
 
 public class GetProjectDTO
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     
     public Project ToEntity()
     {
         return new Project() 
         {
+            Id = Id,
             Name = Name
         };
     }
