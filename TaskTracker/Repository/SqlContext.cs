f8466ae0e1c63ebbe17da6e8284b6c88ded235b2
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-05-29 23:35:47 -0300

    Add ResourceType and Notification entities to DbContext

--- TaskTracker/Repository/SqlContext.cs
+++ TaskTracker/Repository/SqlContext.cs
@@ -10,6 +10,8 @@ public class SqlContext : DbContext
     public DbSet<Task> Tasks { get; set; }
     public DbSet<Resource> Resources { get; set; }
     public DbSet<Project> Projects { get; set; }
+    public DbSet<ResourceType> ResourceTypes { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
     
     public SqlContext(DbContextOptions<SqlContext> options) : base(options)
     {
