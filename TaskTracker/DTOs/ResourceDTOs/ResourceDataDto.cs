0b159e881bf18279507a625ee65ddca86a10383f
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-05-31 16:07:02 -0300

    Add quantity to ResourceDataDto

--- TaskTracker/DTOs/ResourceDTOs/ResourceDataDto.cs
+++ TaskTracker/DTOs/ResourceDTOs/ResourceDataDto.cs
@@ -5,5 +5,5 @@ public class ResourceDataDto
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int TypeResource { get; set; }
-    
+    public int Quantity { get; set; }
 }
