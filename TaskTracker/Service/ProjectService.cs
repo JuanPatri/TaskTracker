f1312d5f303f00e6baa666c71191ba882325602b
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-06-08 16:26:29 -0300

    GREEN: Get projects lead by user

--- TaskTracker/Service/ProjectService.cs
+++ TaskTracker/Service/ProjectService.cs
@@ -388,5 +388,21 @@ public class ProjectService
         return false;
     }
     
-
+    public List<ProjectExporterDataDto> GetProjectsLedByUser(string email){
+        return _projectRepository.FindAll()
+            .Where(p => p.ProjectRoles != null && p.ProjectRoles.Any(pr => pr.RoleType == RoleType.ProjectLead && pr.User.Email == email))
+            .Select(p => new ProjectExporterDataDto
+            {
+                Name = p.Name,
+                StartDate = p.StartDate,
+                Tasks = p.Tasks.Select(t => new TaskExporterDataDto
+                {
+                    Title = t.Title,
+                    StartDate = t.EarlyStart,
+                    Duration = t.Duration,
+                    IsCritical = p.CriticalPath != null && p.CriticalPath.Contains(t) ? "S" : "N",
+                    Resources = t.Resources?.Select(r => r.Resource.Name).ToList() ?? new List<string>()
+                }).ToList()
+            }).ToList();
+    }
 }
