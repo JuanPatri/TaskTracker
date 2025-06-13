7ac73e8285cdb52782480c25f4579201fca37e28
alfonsoricaldoni <AR287195@fi365.ort.edu.uy>
2025-06-12 21:14:51 -0300

    change repository and service user to scoped

--- TaskTracker/Frontend/Program.cs
+++ TaskTracker/Frontend/Program.cs
@@ -20,17 +20,24 @@ builder.Services.AddDbContext<SqlContext>(
         )
     );
 
+// Base de datos en memoria para el resto de entidades
+builder.Services.AddSingleton<InMemoryDatabase>();
+
+//repositories with bata base
+builder.Services.AddScoped<IRepository<User>, UserRepository>(); 
+
 //repositories
-builder.Services.AddSingleton<IRepository<User>, UserRepository>(); 
 builder.Services.AddSingleton<IRepository<Task>, TaskRepository>();
 builder.Services.AddSingleton<IRepository<Project>, ProjectRepository>();
 builder.Services.AddSingleton<IRepository<Resource>, ResourceRepository>();
 builder.Services.AddSingleton<IRepository<ResourceType>, ResourceTypeRepository>();
 builder.Services.AddSingleton<IRepository<Notification>, NotificationRepository>();
 
+//services with database
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<SessionService>();
+
 //services
-builder.Services.AddSingleton<UserService>();
-builder.Services.AddSingleton<SessionService>();
 builder.Services.AddSingleton<ProjectService>();
 builder.Services.AddSingleton<TaskService>();
 builder.Services.AddSingleton<ResourceService>();
