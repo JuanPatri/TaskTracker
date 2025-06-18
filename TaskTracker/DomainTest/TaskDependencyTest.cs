3f9641e7ac4e4331d4eec29ec5e87d80a3d36ee4
Felipe Muzio <138410995+FelipeMuzio@users.noreply.github.com>
2025-06-18 12:14:53 -0300

    GREEN: OnInitialize TaskDependency

---
+++ TaskTracker/DomainTest/TaskDependencyTest.cs
@@ -0,0 +1,18 @@
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task = Domain.Task;
+
+namespace DomainTest;
+
+[TestClass]
+public class TaskDependencyTest
+{
+    private TaskDependency _taskDependency;
+
+    [TestInitialize]
+    public void OnInitialize()
+    {
+        _taskDependency = new TaskDependency();
+    }
+    
+}
